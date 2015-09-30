db38aa7af25bb7f56d6835faabd145e32d9a2376
Steve Koo <shkoo25@gmail.com>
2015-09-30 14:53:20 -0400

    Added Firebase to the System

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,44 @@
 $(document).on("ready", function(){
-	$("button").on("click",function(){
-		console.log("here")
+	var mondayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/monday");
+	
+	$(".monday-button").on("click",function(){
 		var words = $(".text").val();
-		console.log(words)
+		mondayRef.push(words)
+		$(".text").val('');
 
+	})
+	var tuesdayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/tuesday");
+	
+	$(".tuesday-button").on("click",function(){
+		var words = $(".text").val();
+		tuesdayRef.push(words)
+		$(".text").val('');
+	})
+
+	var wednesdayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/wednesday");
+
+	$(".wednesday-button").on("click",function(){
+		var words= $(".text").val();
+		wednesdayRef.push(words)
+		$(".text").val('');
+	})
+
+	var thursdayRef= new Firebase("https://incandescent-heat-3402.firebaseio.com/thursday");
+	$(".thursday-button").on("click",function(){
+		var words=$(".text").val();
+		thursdayRef.push(words)
+		$(".text").val('');
 
 
 	})
+
+	var fridayRef= new Firebase("https://incandescent-heat-3402.firebaseio.com/friday");
+	$(".friday-button").on("click",function(){
+		var words=$(".text").val();
+		fridayRef.push(words)
+		$(".text").val('');
+
+	})
+
 
 })
