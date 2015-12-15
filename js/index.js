1400ca873b3dc20e6dac70d976176f88a3752924
Steve Koo <Steve@Steves-MacBook-Pro.local>
2015-12-15 10:38:11 -0500

    commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$(document).on("ready", function(){
+	var mondayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/monday");
+	
+	$(".monday-button").on("click",function(){
+		var words = $(".text").val();
+		mondayRef.push(words)
+		$(".text").val('');
+		
+
+		mondayRef.on('value',function(snapshot) {
+			console.log(words);
+			//Empty out div here.
+			//for(var task in snapshot.val()){
+				//console.log(snapshot.val())
+				var mondayContainer= $("<div>"+ words + "</div>")
+				$(".monday-container").append(mondayContainer);
+			//}
+			
+
+		})
+		
+
+	})
+	var tuesdayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/tuesday");
+	
+	$(".tuesday-button").on("click",function(){
+		var words2 = $(".text").val();
+		tuesdayRef.push(words2)
+		$(".text").val('');
+
+		tuesdayRef.on('value',function(snapshot){
+			console.log(words2);
+			var tuesdayContainer = $("<div>"+ words2 + "</div>")
+			$(".tuesday-container").append(tuesdayContainer)
+		})
+	})
+
+	var wednesdayRef = new Firebase("https://incandescent-heat-3402.firebaseio.com/wednesday");
+
+	$(".wednesday-button").on("click",function(){
+		var words3= $(".text").val();
+		wednesdayRef.push(words3)
+		$(".text").val('');
+
+		wednesdayRef.on('value',function(snapshot){
+			console.log(words3);
+			var wednesdayContainer = $("<div>"+ words3 + "</div>")
+			$(".wednesday-container").append(wednesdayContainer)
+
+		})
+	})
+
+	var thursdayRef= new Firebase("https://incandescent-heat-3402.firebaseio.com/thursday");
+	$(".thursday-button").on("click",function(){
+		var words=$(".text").val();
+		thursdayRef.push(words)
+		$(".text").val('');
+
+
+	})
+
+	var fridayRef= new Firebase("https://incandescent-heat-3402.firebaseio.com/friday");
+	$(".friday-button").on("click",function(){
+		var words=$(".text").val();
+		fridayRef.push(words)
+		$(".text").val('');
+
+	})
+
+
+})
